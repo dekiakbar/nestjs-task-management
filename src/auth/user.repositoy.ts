daf0e54a0ae9fad0bb4ac7cc498e4a8e864a160d
Deki <dekiakbar@linuxmail.org>
2021-07-20 19:37:26 +0700

    chapter 5, section 4. Error Handling Duplicate Usernames

--- src/auth/user.repositoy.ts
+++ src/auth/user.repositoy.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from './user.entity';
@@ -10,6 +11,16 @@ export class UserRepository extends Repository<User> {
         const user = new User();
         user.username = username;
         user.password = password;
-        await user.save();
+
+        try{
+            await user.save();
+        }catch(error){
+            if( error.code === '23505'){
+                // duplicate username
+                throw new ConflictException('Username already exist');
+            }else{
+                throw new InternalServerErrorException();
+            }
+        }
     }
 }
