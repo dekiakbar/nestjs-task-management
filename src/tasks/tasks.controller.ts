27260b779de8eda5ab33d223086c72438786c91c
Deki <dekiakbar@linuxmail.org>
2021-07-24 01:41:26 +0700

    chapter 6, section 5. Authorization Updating a User's Task Status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -44,11 +44,12 @@ export class TasksController {
         return this.TasksService.createTask(CreateTaskDto, user);
     }
 
-    // @Patch('/:id/status')
-    // updateTaskStatus(
-    //     @Param('id', ParseIntPipe) id: number,
-    //     @Body('status', TaskStatusValidationPipe) status: TaskStatus
-    // ): Promise<Task> {
-    //     return this.TasksService.updateTaskStatus(id, status);
-    // }
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+        @GetUser() user: User
+    ): Promise<Task> {
+        return this.TasksService.updateTaskStatus(id, status, user);
+    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -40,12 +40,16 @@ export class TasksService {
 
     }
 
-    // async updateTaskStatus(id:number, status: TaskStatus): Promise<Task> {
-    //     const task = await this.getTaskById(id);
-    //     task.status = status;
-    //     await task.save();
-    //     return task;
-    // }
+    async updateTaskStatus(
+        id:number,
+        status: TaskStatus,
+        user: User
+    ): Promise<Task> {
+        const task = await this.getTaskById(id, user);
+        task.status = status;
+        await task.save();
+        return task;
+    }
 
     async createTask(
         createTaskDto: CreateTaskDto,
